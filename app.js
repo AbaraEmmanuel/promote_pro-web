d8f4181461748a80be745ae37499ecd4894ea9aa
AbaraEmmanuel <141164356+AbaraEmmanuel@users.noreply.github.com>
2024-11-05 03:15:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 // Firebase configuration
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+// // import { getAuth } from "firebase/auth";
 
-// Firebase initialization
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD4DVbIQUzhNSczujsP27MwTE6NfifB8ew",
-  authDomain: "promote-pro-8f9aa.firebaseapp.com",
-  databaseURL: "https://promote-pro-8f9aa-default-rtdb.firebaseio.com",
-  projectId: "promote-pro-8f9aa",
-  storageBucket: "promote-pro-8f9aa.firebasestorage.app",
-  messagingSenderId: "553030063178",
-  appId: "1:553030063178:web:13e2b89fd5c6c628ccc2b3",
-  measurementId: "G-KZ89FN869W"
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-// const auth = getAuth(app);
+// // Firebase initialization
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD4DVbIQUzhNSczujsP27MwTE6NfifB8ew",
+//   authDomain: "promote-pro-8f9aa.firebaseapp.com",
+//   databaseURL: "https://promote-pro-8f9aa-default-rtdb.firebaseio.com",
+//   projectId: "promote-pro-8f9aa",
+//   storageBucket: "promote-pro-8f9aa.firebasestorage.app",
+//   messagingSenderId: "553030063178",
+//   appId: "1:553030063178:web:13e2b89fd5c6c628ccc2b3",
+//   measurementId: "G-KZ89FN869W"
+// };
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// // const auth = getAuth(app);
 
 window.onload = function() {
     // Check if Telegram WebApp is available
